d21e22d182c16c2b26a2d62b4e67349bd925151b
JovanBV <jovan.brenesv@gmail.com>
2026-03-18 15:07:42 -0600

    fixed navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,18 @@
 import "../styles/NavBar.css"
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import logo from "../assets/paw.svg"
 
 function Navbar() {
+    const toggle = (navData) => {
+    return navData.isActive ? "nav-active" : "nav-inactive";
+    }
+
     return <div className="navbar">
                 <img className="navbar-logo" src={logo}/>
                 <div className="navbar-options">
-                    <Link to={`/`} className="option">Home</Link>
-                    <Link to={`/products`} className="option">Catalog</Link>
-                    <Link to={`/`} className="option">Contact</Link>
+                    <NavLink to={`/`} className={toggle}>Home</NavLink>
+                    <NavLink to={`/products`} className={toggle}>Catalog</NavLink>
+                    <NavLink to={`/contact`} className={toggle}>Contact</NavLink>
                 </div>
             </div>
             }
